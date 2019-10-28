59f5dd8539c173e2e4e3b94b490a596c167f4078
Thiago Martins <contato.thiagocesar@icloud.com>
2019-10-28 08:52:51 -0300

    OrderStatus enum

--- src/main/java/com/course/aulaspring/config/TestConfig.java
+++ src/main/java/com/course/aulaspring/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.course.aulaspring.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.course.aulaspring.entities.Order;
 import com.course.aulaspring.entities.User;
+import com.course.aulaspring.entities.enums.OrderStatus;
+import com.course.aulaspring.repositories.OrderRepository;
 import com.course.aulaspring.repositories.UserRepository;
 
 @Configuration
@@ -15,14 +19,22 @@ import com.course.aulaspring.repositories.UserRepository;
 public class TestConfig  implements CommandLineRunner{
 
 	@Autowired
-	UserRepository userRepository;
+	private UserRepository userRepository;
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
+		
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3 ));
 	}
 	
 	
